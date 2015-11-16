e0e44701c4ed0cb3a8c6f5234a754fec0b2189cf
DemonWav <demonwav@gmail.com>
2015-11-15 22:43:52 -0600

    Updated plugin description

---
+++ src/main/java/com/demonwav/bukkitplugin/BungeeCordModuletype.java
@@ -0,0 +1,35 @@
+package com.demonwav.bukkitplugin;
+
+import com.demonwav.bukkitplugin.icons.BukkitProjectsIcons;
+import com.intellij.openapi.module.JavaModuleType;
+import com.intellij.openapi.module.ModuleTypeManager;
+
+import javax.swing.Icon;
+
+public class BungeeCordModuleType extends JavaModuleType {
+
+    private static final String ID = "BUNGEECORD_MODULE_TYPE";
+
+    public BungeeCordModuleType() {
+        super(ID);
+    }
+
+    public static BungeeCordModuleType getInstance() {
+        return (BungeeCordModuleType) ModuleTypeManager.getInstance().findByID(ID);
+    }
+
+    @Override
+    public Icon getBigIcon() {
+        return BukkitProjectsIcons.SpigotProjectBig;
+    }
+
+    @Override
+    public Icon getIcon() {
+        return BukkitProjectsIcons.SpigotProject;
+    }
+
+    @Override
+    public Icon getNodeIcon(@Deprecated boolean isOpened) {
+        return BukkitProjectsIcons.SpigotProject;
+    }
+}
