c9b3bc12e2b503b26d475237105d8ae491e79b5b
kashike <kashike@vq.lc>
2016-05-03 18:25:39 -0700

    We don't need any of your string, YourKit

--- src/com/demonwav/mcdev/platform/bungeecord/BungeeCordSettings.java
+++ src/com/demonwav/mcdev/platform/bungeecord/BungeeCordSettings.java
@@ -10,7 +10,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
-import com.yourkit.util.Strings;
 
 import java.io.IOException;
 import java.util.List;
