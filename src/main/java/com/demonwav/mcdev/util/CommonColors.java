5fb898bea647049d161d053da16ba6420fb0b887
DemonWav <demonwav@gmail.com>
2016-07-21 22:39:45 -0500

    Set CommonColors fields to private

--- src/main/java/com/demonwav/mcdev/util/CommonColors.java
+++ src/main/java/com/demonwav/mcdev/util/CommonColors.java
@@ -7,22 +7,22 @@ import java.util.Map;
 
 public final class CommonColors {
 
-    public static final Color DARK_RED = new Color(0xAA0000);
-    public static final Color RED = new Color(0xFF5555);
-    public static final Color GOLD = new Color(0xFFAA00);
-    public static final Color YELLOW = new Color(0xFFFF55);
-    public static final Color DARK_GREEN = new Color(0x00AA00);
-    public static final Color GREEN = new Color(0x55FF55);
-    public static final Color AQUA = new Color(0x55FFFF);
-    public static final Color DARK_AQUA = new Color(0x00AAAA);
-    public static final Color DARK_BLUE = new Color(0x0000AA);
-    public static final Color BLUE = new Color(0x5555FF);
-    public static final Color LIGHT_PURPLE = new Color(0xFF55FF);
-    public static final Color DARK_PURPLE = new Color(0xAA00AA);
-    public static final Color WHITE = new Color(0xFFFFFF);
-    public static final Color GRAY = new Color(0xAAAAAA);
-    public static final Color DARK_GRAY = new Color(0x555555);
-    public static final Color BLACK = new Color(0x000000);
+    private static final Color DARK_RED = new Color(0xAA0000);
+    private static final Color RED = new Color(0xFF5555);
+    private static final Color GOLD = new Color(0xFFAA00);
+    private static final Color YELLOW = new Color(0xFFFF55);
+    private static final Color DARK_GREEN = new Color(0x00AA00);
+    private static final Color GREEN = new Color(0x55FF55);
+    private static final Color AQUA = new Color(0x55FFFF);
+    private static final Color DARK_AQUA = new Color(0x00AAAA);
+    private static final Color DARK_BLUE = new Color(0x0000AA);
+    private static final Color BLUE = new Color(0x5555FF);
+    private static final Color LIGHT_PURPLE = new Color(0xFF55FF);
+    private static final Color DARK_PURPLE = new Color(0xAA00AA);
+    private static final Color WHITE = new Color(0xFFFFFF);
+    private static final Color GRAY = new Color(0xAAAAAA);
+    private static final Color DARK_GRAY = new Color(0x555555);
+    private static final Color BLACK = new Color(0x000000);
 
     private CommonColors() {
     }
