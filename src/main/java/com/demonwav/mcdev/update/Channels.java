24a69f1665e7bbcb0942fa71d388a30afa23566f
DemonWav <demonwav@gmail.com>
2016-10-01 23:30:30 -0500

    Add EAP channel to fix things in EAP IntelliJ builds

--- src/main/java/com/demonwav/mcdev/update/Channels.java
+++ src/main/java/com/demonwav/mcdev/update/Channels.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 public enum Channels {
     SCOTLIN("Kotlin And Scala Support", "https://plugins.jetbrains.com/plugins/kotlin%20and%20scala%20support/8327", 1),
-    MIXINS("Mixins", "https://plugins.jetbrains.com/plugins/mixins/8327", 2);
+    MIXINS("Mixins", "https://plugins.jetbrains.com/plugins/mixins/8327", 2),
+    EAP("Early Access Program", "https://plugins.jetbrains.com/plugins/early%20access%20program/8327", 3);
 
     private final String title;
     private final String url;
@@ -44,7 +45,7 @@ public enum Channels {
     }
 
     public static List<Channels> orderedList() {
-        return ImmutableList.of(SCOTLIN, MIXINS);
+        return ImmutableList.of(SCOTLIN, MIXINS, EAP);
     }
 
     public boolean hasChannel() {
