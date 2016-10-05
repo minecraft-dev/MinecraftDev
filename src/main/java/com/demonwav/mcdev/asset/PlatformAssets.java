a10686eced69d0b834fe9547b9e7edb67a39dc9f
DemonWav <demonwav@gmail.com>
2016-10-04 19:33:00 -0500

    Version alpha-0.2.14 Fix missing icon bug (#70)

--- src/main/java/com/demonwav/mcdev/asset/PlatformAssets.java
+++ src/main/java/com/demonwav/mcdev/asset/PlatformAssets.java
@@ -36,10 +36,10 @@ public final class PlatformAssets extends Assets {
     @NotNull public static final Icon LITELOADER_ICON = loadIcon("/assets/icons/platform/LiteLoader.png");
     @NotNull public static final Icon LITELOADER_ICON_2X = loadIcon("/assets/icons/platform/LiteLoader@2x.png");
 
-    @NotNull public static final Icon MIXIN_ICON = loadIcon("/assets/icons/platform/Mixin.png");
-    @NotNull public static final Icon MIXIN_ICON_2X= loadIcon("/assets/icons/platform/Mixin@2x.png");
-    @NotNull public static final Icon MIXIN_ICON_DARK = loadIcon("/assets/icons/platform/Mixin_dark.png");
-    @NotNull public static final Icon MIXIN_ICON_2X_DARK = loadIcon("/assets/icons/platform/Mixin@2x_dark.png");
+    @NotNull public static final Icon MIXIN_ICON = loadIcon("/assets/icons/platform/Mixins.png");
+    @NotNull public static final Icon MIXIN_ICON_2X= loadIcon("/assets/icons/platform/Mixins@2x.png");
+    @NotNull public static final Icon MIXIN_ICON_DARK = loadIcon("/assets/icons/platform/Mixins_dark.png");
+    @NotNull public static final Icon MIXIN_ICON_2X_DARK = loadIcon("/assets/icons/platform/Mixins@2x_dark.png");
 
     @NotNull public static final Icon MCP_ICON = loadIcon("/assets/icons/platform/MCP.png");
     @NotNull public static final Icon MCP_ICON_2X = loadIcon("/assets/icons/platform/MCP@2x.png");
