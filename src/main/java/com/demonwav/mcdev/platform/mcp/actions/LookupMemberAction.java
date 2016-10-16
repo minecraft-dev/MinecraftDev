4044b6cf958b91a4b8a441847df300a5d2741bd9
DemonWav <demonwav@gmail.com>
2016-10-16 01:24:17 -0500

    Create un-implemented lookup MCP members action

---
+++ src/main/java/com/demonwav/mcdev/platform/mcp/actions/LookupMemberAction.java
@@ -0,0 +1,20 @@
+/*
+ * Minecraft Dev for IntelliJ
+ *
+ * https://minecraftdev.org
+ *
+ * Copyright (c) 2016 Kyle Wood (DemonWav)
+ *
+ * MIT License
+ */
+package com.demonwav.mcdev.platform.mcp.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class LookupMemberAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO
+    }
+}
