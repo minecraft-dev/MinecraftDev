54a552a771f398b62b5a5d94e697e5acc10d3e35
DemonWav <demonwav@gmail.com>
2016-11-22 18:58:45 -0600

    Move build script completely over to Kotlin Update license

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,22 @@
-// Not used atm :(
+/*
+ * Minecraft Dev for IntelliJ
+ *
+ * https://minecraftdev.org
+ *
+ * Copyright (c) 2016 minecraft-dev
+ *
+ * MIT License
+ */
+
+import net.minecrell.gradle.licenser.LicenseExtension
+import net.minecrell.gradle.licenser.Licenser
+import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.plugins.ide.idea.IdeaPlugin
 import org.gradle.plugins.ide.idea.model.IdeaModel
+import org.jetbrains.intellij.IntelliJPlugin
 import org.jetbrains.intellij.IntelliJPluginExtension
 import java.io.File
 
@@ -18,46 +33,43 @@ buildscript {
 
     dependencies {
         classpath("gradle.plugin.org.jetbrains:gradle-intellij-plugin:0.1.10")
+        classpath("gradle.plugin.net.minecrell:licenser:0.3")
     }
 }
 
 val ideaVersion by project
 val javaVersion by project
 val pluginVersion by project
+val pluginGroup by project
 val downloadIdeaSources by project
 
 apply {
     plugin<IdeaPlugin>()
-    plugin("org.jetbrains.intellij")
+    plugin<IntelliJPlugin>()
+    plugin<JavaPlugin>()
+    plugin<Licenser>()
 }
 
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-    sourceCompatibility = javaVersion as String
-    targetCompatibility = javaVersion as String
-}
+val java = the<JavaPluginConvention>().sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).java
+java.setSrcDirs(arrayListOf(java.srcDirs, file("gen")))
 
-group = "com.demonwav.minecraft-dev"
-version = pluginVersion
-
-var intellijSandboxDirectory: String? = null
 configure<IntelliJPluginExtension> {
     // IntelliJ IDEA dependency
-    version =  if (project.hasProperty("intellijVersion")){
+    version =  if (project.hasProperty("intellijVersion")) {
         project.properties["intellijVersion"] as String
     } else {
         ideaVersion as String
     }
     // Bundled plugin dependencies
-    setPlugins("maven", "gradle", "Groovy", "yaml", "Kotlin", "org.intellij.scala:2016.2.0")
+    setPlugins("maven", "gradle", "Groovy", "yaml",
+        // needed dependencies for unit tests
+        "properties", "junit")
 
     pluginName = "Minecraft Development"
     updateSinceUntilBuild = false
 
     downloadSources = (downloadIdeaSources as String).toBoolean()
-
     sandboxDirectory = project.rootDir.canonicalPath + "/.sandbox"
-    intellijSandboxDirectory = sandboxDirectory
 }
 
 configure<IdeaModel> {
@@ -67,9 +79,12 @@ configure<IdeaModel> {
     }
 
     module.apply {
-        excludeDirs.add(intellijSandboxDirectory)
+        excludeDirs.add(file(the<IntelliJPluginExtension>().sandboxDirectory))
     }
 }
+
+group = pluginGroup
+version = pluginVersion
 
 val initPropTask = task("initProp") {
     val baseProp = File("src/main/resources/messages.MinecraftDevelopment_en.properties")
@@ -85,12 +100,89 @@ val initPropTask = task("initProp") {
     baseEnglishProp.writeText(comment + baseUsEnglish.readText())
 }
 
-afterEvaluate {
-    val buildPlugin = getTasksByName("buildPlugin", false)
-    val runIdea = getTasksByName("runIdea", false)
+// Credit for this intellij-rust https://github.com/intellij-rust/intellij-rust/blob/master/build.gradle#L114
+val generateAtLexer = task<JavaExec>("generateAtLexer") {
+    val src = "src/main/java/com/demonwav/mcdev/platform/mcp/at/AT.flex"
+    val dst = "gen/com/demonwav/mcdev/platform/mcp/at/"
+    val lexerFileName = "AtLexer.java"
 
-    buildPlugin.forEach { it.dependsOn.addAll(listOf(initPropTask)) }
-    runIdea.forEach { it.dependsOn.addAll(listOf(initPropTask)) }
+    classpath = files("libs/jflex-1.7.0-SNAPSHOT.jar")
+    main = "jflex.Main"
+
+    args(
+        "--skel", "libs/idea-flex.skeleton",
+        "-d", dst,
+        src
+    )
+
+    inputs.file(file(src))
+    inputs.dir(file(dst + lexerFileName))
 }
 
-defaultTasks("buildPlugin")
+/*
+ * This helps us get around the command length issues on Windows by placing the classpath in the manifest of a single
+ * jar, rather than printing them out in one looong line
+ */
+val pathingJar = task<Jar>("pathingJar") {
+    dependsOn(configurations.compile)
+    appendix = "pathing"
+
+    doFirst {
+        manifest.apply {
+            attributes["Class-Path"] = configurations.compile.files.map { file ->
+                file.toURI().toString().replaceFirst("file:/+".toRegex(), "/")
+            }.joinToString(" ")
+        }
+    }
+}
+
+val generateAtPsiAndParser = task<JavaExec>("generateAtPsiAndParser") {
+    dependsOn(pathingJar)
+    doFirst {
+        delete(file("gen/com/demonwav/mcdev/platform/mcp/at/psi/"))
+    }
+
+    val src = "src/main/java/com/demonwav/mcdev/platform/mcp/at/AT.bnf"
+    val dstRoot = "gen"
+
+    main = "org.intellij.grammar.Main"
+
+    args(dstRoot, src)
+
+    inputs.file(file(src))
+    outputs.dir(fileTree(mapOf(
+        "dir" to dstRoot + "/com/demonwav/mcdev/platform/mcp/at/",
+        "include" to "**/*.java"
+    )))
+
+    classpath("$buildDir/classes/main", "$buildDir/resources/main", pathingJar.archivePath, fileTree(mapOf(
+        "dir" to "libs/",
+        "include" to "**/*.jar"
+    )))
+}
+
+val generate = task("generate") {
+    dependsOn(generateAtLexer, generateAtPsiAndParser)
+}
+
+tasks.withType<JavaCompile> {
+    dependsOn(generate)
+    options.encoding = "UTF-8"
+    sourceCompatibility = javaVersion as String
+    targetCompatibility = javaVersion as String
+}
+
+// License header formatting
+configure<LicenseExtension> {
+    header = file("copyright.txt")
+    include("**/*.java", "**/*.gradle", "**/*.xml", "**/*.properties", "**/*.html")
+    exclude("gen/**", "**messages.MinecraftDevelopment.properties", "**messages.MinecraftDevelopment_en.properties")
+
+    newLine = true
+}
+
+afterEvaluate {
+    getTasksByName("prepareSandbox", false).forEach { it.dependsOn("initProp") }
+}
+
+defaultTasks("clean", "buildPlugin", "test")
