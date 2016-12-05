5075c9e1f2d838b8bbffc0b3bf0d9c0140bc04ea
DemonWav <demonwav@gmail.com>
2016-12-04 21:27:51 -0600

    Minor cleanup of CommonColors

--- src/main/kotlin/com/demonwav/mcdev/util/CommonColors.kt
+++ src/main/kotlin/com/demonwav/mcdev/util/CommonColors.kt
@@ -33,21 +33,23 @@ object CommonColors {
 
     @JvmStatic
     fun applyStandardColors(map: MutableMap<String, Color>, prefix: String) {
-        map.put(prefix + ".DARK_RED", DARK_RED)
-        map.put(prefix + ".RED", RED)
-        map.put(prefix + ".GOLD", GOLD)
-        map.put(prefix + ".YELLOW", YELLOW)
-        map.put(prefix + ".DARK_GREEN", DARK_GREEN)
-        map.put(prefix + ".GREEN", GREEN)
-        map.put(prefix + ".AQUA", AQUA)
-        map.put(prefix + ".DARK_AQUA", DARK_AQUA)
-        map.put(prefix + ".DARK_BLUE", DARK_BLUE)
-        map.put(prefix + ".BLUE", BLUE)
-        map.put(prefix + ".LIGHT_PURPLE", LIGHT_PURPLE)
-        map.put(prefix + ".DARK_PURPLE", DARK_PURPLE)
-        map.put(prefix + ".WHITE", WHITE)
-        map.put(prefix + ".GRAY", GRAY)
-        map.put(prefix + ".DARK_GRAY", DARK_GRAY)
-        map.put(prefix + ".BLACK", BLACK)
+        map.apply {
+            put("$prefix.DARK_RED", DARK_RED)
+            put("$prefix.RED", RED)
+            put("$prefix.GOLD", GOLD)
+            put("$prefix.YELLOW", YELLOW)
+            put("$prefix.DARK_GREEN", DARK_GREEN)
+            put("$prefix.GREEN", GREEN)
+            put("$prefix.AQUA", AQUA)
+            put("$prefix.DARK_AQUA", DARK_AQUA)
+            put("$prefix.DARK_BLUE", DARK_BLUE)
+            put("$prefix.BLUE", BLUE)
+            put("$prefix.LIGHT_PURPLE", LIGHT_PURPLE)
+            put("$prefix.DARK_PURPLE", DARK_PURPLE)
+            put("$prefix.WHITE", WHITE)
+            put("$prefix.GRAY", GRAY)
+            put("$prefix.DARK_GRAY", DARK_GRAY)
+            put("$prefix.BLACK", BLACK)
+        }
     }
 }
