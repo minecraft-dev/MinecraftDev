5b7ab9e5ba8147e7beee4fe09ee0253e311e3633
DemonWav <demonwav@gmail.com>
2017-01-26 12:12:13 -0600

    Exclude Mixins from Entity constructor checks (Fixes #142)

---
+++ src/main/kotlin/com/demonwav/mcdev/platform/mcp/inspections/EntityConstructorInspection.kt
@@ -0,0 +1,85 @@
+/*
+ * Minecraft Dev for IntelliJ
+ *
+ * https://minecraftdev.org
+ *
+ * Copyright (c) 2017 minecraft-dev
+ *
+ * MIT License
+ */
+
+package com.demonwav.mcdev.platform.mcp.inspections
+
+import com.demonwav.mcdev.platform.MinecraftModule
+import com.demonwav.mcdev.platform.mcp.McpModuleType
+import com.demonwav.mcdev.platform.mcp.util.McpConstants
+import com.demonwav.mcdev.platform.mixin.util.MixinUtils
+import com.demonwav.mcdev.util.extendsOrImplementsClass
+import com.intellij.openapi.module.ModuleUtilCore
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiClassType
+import com.intellij.psi.PsiTypeParameter
+import com.siyeh.ig.BaseInspection
+import com.siyeh.ig.BaseInspectionVisitor
+import org.jetbrains.annotations.Nls
+
+class EntityConstructorInspection : BaseInspection() {
+    @Nls
+    override fun getDisplayName(): String {
+        return "MCP Entity class missing World constructor"
+    }
+
+    override fun buildErrorString(vararg infos: Any): String {
+        return "All entities must have a constructor that takes one " + McpConstants.WORLD + " parameter."
+    }
+
+    override fun buildVisitor(): BaseInspectionVisitor {
+        return object : BaseInspectionVisitor() {
+            override fun visitClass(aClass: PsiClass) {
+                if (aClass is PsiTypeParameter) {
+                    return
+                }
+
+                if (!extendsOrImplementsClass(aClass, McpConstants.ENTITY)) {
+                    return
+                }
+
+                val module = ModuleUtilCore.findModuleForPsiElement(aClass) ?: return
+
+                val instance = MinecraftModule.getInstance(module) ?: return
+
+                if (!instance.isOfType(McpModuleType.getInstance())) {
+                    return
+                }
+
+                if (!MixinUtils.getAllMixedClasses(aClass).isEmpty()) {
+                    return
+                }
+
+                val constructors = aClass.constructors
+                for (constructor in constructors) {
+                    if (constructor.parameterList.parameters.size != 1) {
+                        continue
+                    }
+
+                    val parameter = constructor.parameterList.parameters[0]
+                    val typeElement = parameter.typeElement ?: continue
+
+                    val type = typeElement.type as? PsiClassType ?: continue
+
+                    val resolve = type.resolve() ?: continue
+
+                    if (resolve.qualifiedName == null) {
+                        continue
+                    }
+
+                    if (resolve.qualifiedName == McpConstants.WORLD) {
+                        return
+                    }
+                }
+
+                registerClassError(aClass)
+            }
+        }
+    }
+}
