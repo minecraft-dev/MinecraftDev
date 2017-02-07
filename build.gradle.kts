8c1b5cbf74ae5ae5ebeb6973ceb557684f7f0f9c
DemonWav <demonwav@gmail.com>
2017-02-07 12:57:45 -0600

    Rename Grammar Kit to 1.5.1-SNAPSHOT to make it less ambiguous.

--- build.gradle.kts
+++ build.gradle.kts
@@ -201,7 +201,7 @@ val generateAtPsiAndParser = task<JavaExec>("generateAtPsiAndParser") {
         "parser" to parserDir
     ))
 
-    classpath(pathingJar.archivePath, file("libs/grammar-kit-1.5.0.jar"))
+    classpath(pathingJar.archivePath, file("libs/grammar-kit-1.5.1-SNAPSHOT.jar"))
 }
 
 val generate = task("generate") {
