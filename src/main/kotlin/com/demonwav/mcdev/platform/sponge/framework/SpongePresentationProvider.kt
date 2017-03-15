8c8b6a34d4bc82990d67a4d3dd60cbdd2a167177
DemonWav <demonwav@gmail.com>
2017-03-14 19:10:47 -0500

    Use the correct version manifest entry based on the present SpongeAPI entry Fixes #180

--- src/main/kotlin/com/demonwav/mcdev/platform/sponge/framework/SpongePresentationProvider.kt
+++ src/main/kotlin/com/demonwav/mcdev/platform/sponge/framework/SpongePresentationProvider.kt
@@ -14,10 +14,10 @@ import com.demonwav.mcdev.asset.PlatformAssets
 import com.intellij.framework.library.LibraryVersionProperties
 import com.intellij.openapi.roots.libraries.LibraryPresentationProvider
 import com.intellij.openapi.roots.libraries.LibraryProperties
-import com.intellij.openapi.util.io.JarUtil
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import java.util.jar.Attributes
+import java.util.jar.JarFile
 
 class SpongePresentationProvider : LibraryPresentationProvider<LibraryVersionProperties>(SPONGE_LIBRARY_KIND) {
 
@@ -26,14 +26,17 @@ class SpongePresentationProvider : LibraryPresentationProvider<LibraryVersionPro
     override fun detect(classesRoots: List<VirtualFile>): LibraryVersionProperties? {
         loop@ for (classesRoot in classesRoots) {
             val file = VfsUtilCore.virtualToIoFile(classesRoot)
+            val manifest = JarFile(file).manifest ?: continue
 
-            when ("SpongeAPI") {
-                JarUtil.getJarAttribute(file, Attributes.Name.IMPLEMENTATION_TITLE) -> {}
-                JarUtil.getJarAttribute(file, Attributes.Name.SPECIFICATION_TITLE) -> {}
+            val mainAttributes = manifest.mainAttributes
+
+            val versionAttribute = when ("SpongeAPI") {
+                mainAttributes.getValue(Attributes.Name.IMPLEMENTATION_TITLE) -> Attributes.Name.IMPLEMENTATION_VERSION
+                mainAttributes.getValue(Attributes.Name.SPECIFICATION_TITLE) -> Attributes.Name.SPECIFICATION_VERSION
                 else -> continue@loop
             }
 
-            val version = JarUtil.getJarAttribute(file, Attributes.Name.IMPLEMENTATION_VERSION) ?: continue
+            val version = mainAttributes.getValue(versionAttribute) ?: continue
             return LibraryVersionProperties(version)
         }
         return null
