81aac8b991e640245dd4f3e907603dff61a82d50
Minecrell <dev@minecrell.net>
2017-04-02 11:31:29 +0200

    mixin: Make overwrite modifiers inspection less confusing

--- src/main/kotlin/com/demonwav/mcdev/platform/mixin/inspection/overwrite/OverwriteModifiersInspection.kt
+++ src/main/kotlin/com/demonwav/mcdev/platform/mixin/inspection/overwrite/OverwriteModifiersInspection.kt
@@ -12,14 +12,17 @@ package com.demonwav.mcdev.platform.mixin.inspection.overwrite
 
 import com.demonwav.mcdev.platform.mixin.util.mixinTargets
 import com.demonwav.mcdev.platform.mixin.util.resolveFirstOverwriteTarget
+import com.demonwav.mcdev.util.findKeyword
 import com.demonwav.mcdev.util.ifEmpty
 import com.demonwav.mcdev.util.isAccessModifier
 import com.intellij.codeInsight.intention.AddAnnotationFix
 import com.intellij.codeInsight.intention.QuickFixFactory
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiAnnotation
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiModifier
+import com.intellij.psi.util.PsiUtil
 
 class OverwriteModifiersInspection : OverwriteInspection() {
 
@@ -30,24 +33,32 @@ class OverwriteModifiersInspection : OverwriteInspection() {
         val targetClasses = psiClass.mixinTargets.ifEmpty { return }
 
         val target = resolveFirstOverwriteTarget(targetClasses, method)?.modifierList ?: return
+        val nameIdentifier = method.nameIdentifier ?: return
         val modifierList = method.modifierList
 
         for (modifier in PsiModifier.MODIFIERS) {
             val targetModifier = target.hasModifierProperty(modifier)
             val overwriteModifier = modifierList.hasModifierProperty(modifier)
             if (targetModifier != overwriteModifier) {
-                if (!targetModifier && isAccessModifier(modifier)) {
-                    // Don't attempt to remove access modifiers
-                    continue
-                }
+                val target: PsiElement
+                val message = if (isAccessModifier(modifier)) {
+                    if (!targetModifier) {
+                        // Don't attempt to remove access modifiers
+                        continue
+                    }
 
-                val message = if (targetModifier) {
-                    "'$modifier' is not present on target method"
+                    val currentModifier = PsiUtil.getAccessModifier(PsiUtil.getAccessLevel(modifierList))
+                    target = modifierList.findKeyword(currentModifier) ?: nameIdentifier
+                    "Invalid access modifiers, has: $currentModifier, but target has: $overwriteModifier"
+                } else if (targetModifier) {
+                    target = nameIdentifier
+                    "Method must be '$modifier'"
                 } else {
+                    target = modifierList.findKeyword(modifier) ?: nameIdentifier
                     "'$modifier' modifier does not match target method"
                 }
 
-                holder.registerProblem(modifierList, message,
+                holder.registerProblem(target, message,
                     QuickFixFactory.getInstance().createModifierListFix(modifierList, modifier, targetModifier, false))
             }
         }
@@ -56,7 +67,7 @@ class OverwriteModifiersInspection : OverwriteInspection() {
             val qualifiedName = annotation.qualifiedName ?: continue
             val overwriteAnnotation = modifierList.findAnnotation(qualifiedName)
             if (overwriteAnnotation == null) {
-                holder.registerProblem(modifierList, "Missing @${annotation.nameReferenceElement?.text} annotation",
+                holder.registerProblem(nameIdentifier, "Missing @${annotation.nameReferenceElement?.text} annotation",
                     AddAnnotationFix(qualifiedName, method, annotation.parameterList.attributes))
             }
 
