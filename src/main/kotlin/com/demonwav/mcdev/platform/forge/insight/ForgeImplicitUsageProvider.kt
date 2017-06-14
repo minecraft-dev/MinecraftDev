a06ca685a7caf8f937fe86228a6e7aceba747bc2
Minecrell <dev@minecrell.net>
2017-06-14 17:16:51 +0200

    Add ForgeImplicitUsageProvider to mark coremod classes as used
    Closes #237

---
+++ src/main/kotlin/com/demonwav/mcdev/platform/forge/insight/ForgeImplicitUsageProvider.kt
@@ -0,0 +1,30 @@
+/*
+ * Minecraft Dev for IntelliJ
+ *
+ * https://minecraftdev.org
+ *
+ * Copyright (c) 2017 minecraft-dev
+ *
+ * MIT License
+ */
+
+package com.demonwav.mcdev.platform.forge.insight
+
+import com.demonwav.mcdev.platform.forge.util.ForgeConstants
+import com.demonwav.mcdev.util.extendsOrImplements
+import com.intellij.codeInsight.daemon.ImplicitUsageProvider
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
+
+class ForgeImplicitUsageProvider : ImplicitUsageProvider {
+
+    override fun isImplicitUsage(element: PsiElement) = isCoreMod(element)
+
+    fun isCoreMod(element: PsiElement): Boolean {
+        return element is PsiClass && element.extendsOrImplements(ForgeConstants.CORE_MOD_INTERFACE)
+    }
+
+    override fun isImplicitRead(element: PsiElement) = false
+    override fun isImplicitWrite(element: PsiElement) = false
+
+}
