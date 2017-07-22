ace07dca645cc5dc1c2753d9a729110510b8cbeb
Marvin Rösch <marvinroesch99@gmail.com>
2017-07-22 18:10:52 +0200

    Complete parsing tests

--- src/test/kotlin/com/demonwav/mcdev/i18n/I18nParsingTest.kt
+++ src/test/kotlin/com/demonwav/mcdev/i18n/I18nParsingTest.kt
@@ -15,9 +15,10 @@ import com.demonwav.mcdev.i18n.lang.I18nParserDefintion
 import com.intellij.testFramework.ParsingTestCase
 
 class I18nParsingTest : ParsingTestCase("com/demonwav/mcdev/i18n/parser/fixtures", "lang", true, I18nParserDefintion()) {
-
     override fun getTestDataPath() = "src/test/resources"
     override fun getTestName(lowercaseFirstLetter: Boolean) = super.getTestName(lowercaseFirstLetter).toSnakeCase()
 
     fun testProperties() = doTest(true)
+    fun testComments() = doTest(true)
+    fun testMixed() = doTest(true)
 }
