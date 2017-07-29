a79a4517eb1879f4b84dd8dedbc05fe40e1e78ee
DemonWav <demonwav@gmail.com>
2017-07-29 16:14:25 -0500

    Upgrade Mixin dependency to 0.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,7 +113,7 @@ dependencies {
     "grammar-kit"("org.jetbrains.idea:grammar-kit:1.5.1")
 
     "testLibs"("org.jetbrains.idea:mockJDK:1.7-4d76c50")
-    "testLibs"("org.spongepowered:mixin:0.6.8-SNAPSHOT:thin")
+    "testLibs"("org.spongepowered:mixin:0.7-SNAPSHOT:thin")
 }
 
 intellij {
