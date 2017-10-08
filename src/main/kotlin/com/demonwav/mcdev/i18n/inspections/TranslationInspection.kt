811d222a3af7587ca05ab7f4e8ea2d2bf6a2659d
Marvin Rösch <marvinroesch99@gmail.com>
2017-10-08 11:24:15 +0200

    Check for MCP module type for translation inspections

--- src/main/kotlin/com/demonwav/mcdev/i18n/inspections/TranslationInspection.kt
+++ src/main/kotlin/com/demonwav/mcdev/i18n/inspections/TranslationInspection.kt
@@ -10,14 +10,30 @@
 
 package com.demonwav.mcdev.i18n.inspections
 
+import com.demonwav.mcdev.platform.mcp.McpModuleType
 import com.intellij.codeInspection.BaseJavaLocalInspectionTool
+import com.intellij.codeInspection.InspectionManager
+import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiFile
 
 abstract class TranslationInspection : BaseJavaLocalInspectionTool() {
     protected abstract fun buildVisitor(holder: ProblemsHolder): PsiElementVisitor
 
     override final fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
-        return buildVisitor(holder)
+        if (McpModuleType.isInModule(holder.file)) {
+            return buildVisitor(holder)
+        }
+
+        return PsiElementVisitor.EMPTY_VISITOR
+    }
+
+    override final fun processFile(file: PsiFile, manager: InspectionManager): List<ProblemDescriptor> {
+        return if (McpModuleType.isInModule(file)) {
+            super.processFile(file, manager)
+        } else {
+            listOf()
+        }
     }
 }
