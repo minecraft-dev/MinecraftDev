1f3d49610c82876df581747a1f887f01b29a8e68
kashike <kashike@vq.lc>
2017-11-10 23:09:22 -0800

    Don't display a byte tag as true/false for a value of 1/0

--- src/main/kotlin/com/demonwav/mcdev/nbt/tags/TagByte.kt
+++ src/main/kotlin/com/demonwav/mcdev/nbt/tags/TagByte.kt
@@ -22,16 +22,5 @@ class TagByte(override val value: Byte) : NbtValueTag<Byte>(Byte::class.java) {
 
     override fun toString() = toString(StringBuilder(), 0, WriterState.COMPOUND).toString()
 
-    override fun toString(sb: StringBuilder, indentLevel: Int, writerState: WriterState): StringBuilder {
-        if (writerState == WriterState.LIST) {
-            return sb.append(value).append('B')
-        }
-
-        if (value.toInt() == 1) {
-            return sb.append("true")
-        } else if (value.toInt() == 0) {
-            return sb.append("false")
-        }
-        return sb.append(value).append('B')
-    }
+    override fun toString(sb: StringBuilder, indentLevel: Int, writerState: WriterState) = sb.append(value).append('B')!!
 }
