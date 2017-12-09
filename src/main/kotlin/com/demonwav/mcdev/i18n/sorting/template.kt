938290d3d7196e06790f2b9d1784c1fb0e4b7759
Marvin Rösch <marvinroesch99@gmail.com>
2017-12-09 20:34:23 +0100

    Simplify quantifier parsing

--- src/main/kotlin/com/demonwav/mcdev/i18n/sorting/template.kt
+++ src/main/kotlin/com/demonwav/mcdev/i18n/sorting/template.kt
@@ -41,7 +41,7 @@ data class Template(val elements: List<TemplateElement>) {
                 }
             )
 
-        private val keyRegex = "(![+*]?|\\?[+*]?|[+*])?([^+*!?]*)(![+*]?|\\?[+*]?|[+*])?".toRegex()
+        private val keyRegex = "([?!]?[+*]?)([^+*!?]*)([?!]?[+*]?)".toRegex()
 
         private fun parseKey(s: String) =
             keyRegex.findAll(s).map {
@@ -59,7 +59,7 @@ data class Template(val elements: List<TemplateElement>) {
 
                 "+", "?*" -> "(.+)"
                 "*" -> "(.*?)"
-                
+
                 else -> ""
             }
 
