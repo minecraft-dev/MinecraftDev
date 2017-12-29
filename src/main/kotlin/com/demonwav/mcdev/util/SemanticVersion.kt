d392f7bdaca8c6cb1d5e492db3761d3264494203
Marvin Rösch <marvinroesch99@gmail.com>
2017-12-29 14:40:52 +0100

    Handle arbitrary text parts in versions

--- src/main/kotlin/com/demonwav/mcdev/util/SemanticVersion.kt
+++ src/main/kotlin/com/demonwav/mcdev/util/SemanticVersion.kt
@@ -12,6 +12,7 @@ package com.demonwav.mcdev.util
 
 import com.demonwav.mcdev.util.SemanticVersion.Companion.VersionPart.PreReleasePart
 import com.demonwav.mcdev.util.SemanticVersion.Companion.VersionPart.ReleasePart
+import com.demonwav.mcdev.util.SemanticVersion.Companion.VersionPart.TextPart
 
 /**
  * Represents a comparable and generalised "semantic version".
@@ -24,7 +25,7 @@ class SemanticVersion(val parts: List<VersionPart>) : Comparable<SemanticVersion
     override fun compareTo(other: SemanticVersion): Int {
         // Zipping limits the compared parts to the shorter version, then we perform a component-wise comparison
         // Short-circuits if any component of this version is smaller/older
-        val result = parts.zip(other.parts).fold(0) { acc, (a, b) -> if (acc == -1) acc else a.compareTo(b) }
+        val result = parts.zip(other.parts).fold(0) { acc, (a, b) -> if (acc != 0) acc else a.compareTo(b) }
         // When all the parts are equal, the longer version wins
         // Generally speaking, 1.0 is considered older than 1.0.1 (see MC 1.12 vs 1.12.1)
         if (parts.size != other.parts.size && result == 0) {
@@ -42,6 +43,15 @@ class SemanticVersion(val parts: List<VersionPart>) : Comparable<SemanticVersion
     override fun hashCode() = parts.hashCode()
 
     companion object {
+        /**
+         * The higher the priority value associated with a certain part, the higher it is ranked in comparisons.
+         * Unknown parts will always be "rated" lower and equal among each other.
+         */
+        val TEXT_PRIORITIES = mapOf(
+            "snapshot" to 0,
+            "rc" to 1
+        )
+
         /**
          * Parses a version string into a comparable representation.
          * @throws IllegalArgumentException if any part of the version string cannot be parsed as integer or split into pre-release parts.
@@ -63,6 +73,12 @@ class SemanticVersion(val parts: List<VersionPart>) : Comparable<SemanticVersion
                     } else {
                         throw IllegalArgumentException("Failed to split pre-release version part into two numbers: $it")
                     }
+                } else if (it.contains("-")) {
+                    // We might consume snapshot versions as well
+                    // Those are generally rated the lowest in comparisons
+                    // As a simplification, treat anything separated by a dash like that
+                    val subParts = it.split("-", limit = 2)
+                    TextPart(parseInt(subParts[0]), subParts[1])
                 } else {
                     ReleasePart(parseInt(it))
                 }
@@ -78,8 +94,9 @@ class SemanticVersion(val parts: List<VersionPart>) : Comparable<SemanticVersion
 
                 override fun compareTo(other: VersionPart) =
                     when (other) {
-                        is PreReleasePart -> if (version != other.version) version - other.version else 1
                         is ReleasePart -> version - other.version
+                        is PreReleasePart -> if (version != other.version) version - other.version else 1
+                        is TextPart -> if (version != other.version) version - other.version else 1
                     }
             }
 
@@ -88,8 +105,22 @@ class SemanticVersion(val parts: List<VersionPart>) : Comparable<SemanticVersion
 
                 override fun compareTo(other: VersionPart) =
                     when (other) {
-                        is PreReleasePart -> if (version != other.version) version - other.version else pre - other.pre
                         is ReleasePart -> if (version != other.version) version - other.version else -1
+                        is PreReleasePart -> if (version != other.version) version - other.version else pre - other.pre
+                        is TextPart -> if (version != other.version) version - other.version else 1
+                    }
+            }
+
+            data class TextPart(val version: Int, val text: String) : VersionPart() {
+                private val priority = TEXT_PRIORITIES[text.toLowerCase()] ?: -1
+
+                override val versionString = "$version-$text"
+
+                override fun compareTo(other: VersionPart) =
+                    when (other) {
+                        is ReleasePart -> if (version != other.version) version - other.version else -1
+                        is PreReleasePart -> if (version != other.version) version - other.version else -1
+                        is TextPart -> if (version != other.version) version - other.version else priority - other.priority
                     }
             }
         }
