2a4e11e30058e951b1fe5537d37bc46d1ebaa61f
Kyle Wood <demonwav@gmail.com>
2018-02-18 20:59:36 -0600

    Oops

--- src/main/kotlin/com/demonwav/mcdev/platform/mixin/inspection/implements/SoftImplementOverridesInspection.kt
+++ src/main/kotlin/com/demonwav/mcdev/platform/mixin/inspection/implements/SoftImplementOverridesInspection.kt
@@ -17,7 +17,7 @@ import com.intellij.psi.JavaElementVisitor
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiMethod
 
-class SuSoftImplementOverridesInspection : MixinInspection() {
+class git statSoftImplementOverridesInspection : MixinInspection() {
 
     override fun getStaticDescription() = "Reports soft-implemented methods in Mixins that do not override a method in the target classes."
 
