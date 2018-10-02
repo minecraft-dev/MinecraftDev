8b5ec41de0d0121c5dab3371035b04ebfae352f3
Zach Brown <zach.brown@destroystokyo.com>
2018-10-02 19:18:21 -0400

    Add API Version flag for 1.13+ Bukkit plugins
    Fixes GH-453

--- src/main/kotlin/com/demonwav/mcdev/platform/bukkit/BukkitTemplate.kt
+++ src/main/kotlin/com/demonwav/mcdev/platform/bukkit/BukkitTemplate.kt
@@ -101,6 +101,14 @@ object BukkitTemplate {
             properties.setProperty("HAS_WEBSITE", "true")
         }
 
+        // Plugins targeting 1.13 or newer need an explicit api declaration flag
+        // Unfortunately this flag has no contract to match any specific API version
+        val mcVer = settings.minecraftVersion.substring(0, 4).toDoubleOrNull()
+        if (mcVer != null && mcVer >= 1.13) {
+            properties.setProperty("API_VERSION", "1.13")
+            properties.setProperty("HAS_API_VERSION", "true")
+        }
+
         BaseTemplate.applyTemplate(project, file, MinecraftFileTemplateGroupFactory.BUKKIT_PLUGIN_YML_TEMPLATE, properties, true)
     }
 }
