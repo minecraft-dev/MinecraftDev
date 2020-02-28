b2e0f4b17e75489ab82d18c18a67b661050df9e8
Joe <burtonjae@hotmail.co.uk>
2020-02-28 16:20:44 +0000

    Handle invokers for constructors

--- src/main/kotlin/com/demonwav/mcdev/platform/mixin/util/Invoker.kt
+++ src/main/kotlin/com/demonwav/mcdev/platform/mixin/util/Invoker.kt
@@ -19,6 +19,7 @@ import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiMember
 import com.intellij.psi.PsiMethod
+import com.intellij.psi.util.PsiUtil
 import com.intellij.psi.SmartPsiElementPointer
 import com.intellij.psi.util.createSmartPointer
 import java.util.Locale
@@ -36,8 +37,18 @@ fun resolveInvokerTarget(
     member: PsiMember
 ): PsiMember? {
     val name = getInvokerTargetName(invoker, member) ?: return null
+    val constructor = name == "<init>"
     return when (member) {
-        is PsiMethod -> targetClasses.mapFirstNotNull { it.findMatchingMethod(member, false, name) }
+        is PsiMethod -> targetClasses.mapFirstNotNull {
+            if (constructor && PsiUtil.resolveClassInType(member.returnType)?.qualifiedName != it.qualifiedName)
+                return null
+            it.findMatchingMethod(
+                member,
+                false,
+                if (constructor) it.name ?: name else name,
+                constructor
+            )
+        }
         else -> null
     }
 }
