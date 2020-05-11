4fc91b2ab726847457009eb0549db46d4d21cdaf
darbyjack <admin@glaremasters.me>
2020-05-10 21:42:51 -0500

    Fixed constants

--- src/main/kotlin/com/demonwav/mcdev/platform/placeholderapi/framework/PlaceholderApiPresentationProvider.kt
+++ src/main/kotlin/com/demonwav/mcdev/platform/placeholderapi/framework/PlaceholderApiPresentationProvider.kt
@@ -11,10 +11,24 @@
 package com.demonwav.mcdev.platform.placeholderapi.framework
 
 import com.demonwav.mcdev.asset.PlatformAssets
-import com.demonwav.mcdev.facet.ManifestLibraryPresentationProvider
+import com.demonwav.mcdev.platform.placeholderapi.util.PlaceholderApiConstants
+import com.demonwav.mcdev.util.localFile
 import com.intellij.framework.library.LibraryVersionProperties
+import com.intellij.openapi.roots.libraries.LibraryPresentationProvider
+import com.intellij.openapi.util.io.JarUtil
+import com.intellij.openapi.vfs.VirtualFile
 
-class PlaceholderApiPresentationProvider :
-    ManifestLibraryPresentationProvider(PLACEHOLDERAPI_LIBRARY_KIND, "PlaceholderAPI", true) {
+class PlaceholderApiPresentationProvider : LibraryPresentationProvider<LibraryVersionProperties>(PLACEHOLDERAPI_LIBRARY_KIND) {
     override fun getIcon(properties: LibraryVersionProperties?) = PlatformAssets.PLACEHOLDERAPI_ICON
+
+    override fun detect(classesRoots: MutableList<VirtualFile>): LibraryVersionProperties? {
+        for (classesRoot in classesRoots) {
+            val file = classesRoot.localFile
+
+            if (JarUtil.containsClass(file, PlaceholderApiConstants.EXPANSION_CLASS)) {
+                return LibraryVersionProperties()
+            }
+        }
+        return null
+    }
 }
