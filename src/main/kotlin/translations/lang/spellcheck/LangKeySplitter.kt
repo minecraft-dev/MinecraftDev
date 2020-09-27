d47811259d6cb6445d48e5e1c2ba2e7b7e59baef
SizableShrimp <29855886+SizableShrimp@users.noreply.github.com>
2020-09-26 21:46:13 -0500

    Add spellchecking strategy for lang files This includes support for translation keys, translation values, and comments.

---
+++ src/main/kotlin/translations/lang/spellcheck/LangKeySplitter.kt
@@ -0,0 +1,27 @@
+package com.demonwav.mcdev.translations.lang.spellcheck
+
+import com.intellij.openapi.util.TextRange
+import com.intellij.spellchecker.inspections.BaseSplitter
+import com.intellij.spellchecker.inspections.IdentifierSplitter
+import com.intellij.util.Consumer
+
+object LangKeySplitter : BaseSplitter() {
+    override fun split(text: String?, range: TextRange, consumer: Consumer<TextRange>?) {
+        if (text == null) {
+            return
+        }
+
+        val subText = newBombedCharSequence(text, range)
+        val codepoints = subText.split('.')
+        val idSplitter = IdentifierSplitter.getInstance()
+
+        var index = range.startOffset
+        for (codepoint in codepoints) {
+            checkCancelled()
+            // Use full text with the correct range
+            idSplitter.split(text, TextRange.from(index, codepoint.length), consumer)
+
+            index += codepoint.length + 1 // Add the length of the codepoint plus 1 for the period
+        }
+    }
+}
