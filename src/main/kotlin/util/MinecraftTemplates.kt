d70fd6e7a10cf65d4ee7572dc78ec6530e6cbf35
Mysterious_Dev <mysteriousdevs@gmail.com>
2021-02-07 14:05:44 +0100

    Add Fabric Enchantment class template

--- src/main/kotlin/util/MinecraftTemplates.kt
+++ src/main/kotlin/util/MinecraftTemplates.kt
@@ -237,5 +237,6 @@ class MinecraftTemplates : FileTemplateGroupDescriptorFactory {
 
         const val FABRIC_BLOCK_TEMPLATE = "FabricBlock.java"
         const val FABRIC_ITEM_TEMPLATE = "FabricItem.java"
+        const val FABRIC_ENCHANTMENT_TEMPLATE = "FabricEnchantment.java"
     }
 }
