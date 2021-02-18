6ee62fceaf865a9251509adda851ec60a46fae1a
lego3708 <l3gomindstorms@gmail.com>
2021-02-18 10:26:43 +0000

    Add test for ModifyArgs and depend on non-thin version of Mixin for now.

---
+++ src/test/resources/com/demonwav/mcdev/platform/mixin/invalidInjectorMethodSignature/modifyArgs.after.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(MixedInSimple.class)
+public class TestMixin {
+
+    @ModifyArgs(method = "simpleMethod", at = @At(value = "INVOKE", target = "Ljava/lang/Integer;parseInt(Ljava/lang/String;I)I"))
+    private void inject(Args args<caret>) {
+    }
+}
