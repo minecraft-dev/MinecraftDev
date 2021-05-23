74f0c4a4c83929b2ac900e4fc65a6eca52c56221
SizableShrimp <sizableshrimp@sizableshrimp.me>
2021-05-23 06:38:31 -0500

    Fix AT generation for members in subclasses

--- src/main/kotlin/platform/mcp/actions/CopyAtAction.kt
+++ src/main/kotlin/platform/mcp/actions/CopyAtAction.kt
@@ -25,19 +25,25 @@ class CopyAtAction : SrgActionBase() {
     override fun withSrgTarget(parent: PsiElement, srgMap: McpSrgMap, e: AnActionEvent, data: ActionData) {
         when (parent) {
             is PsiField -> {
+                if (parent.containingClass == null)
+                    return showBalloon("No SRG name found", e)
+                val classSrg = srgMap.getSrgClass(parent.containingClass!!) ?: return showBalloon("No SRG name found", e)
                 val srg = srgMap.getSrgField(parent) ?: return showBalloon("No SRG name found", e)
                 copyToClipboard(
                     data.editor,
                     data.element,
-                    parent.containingClass?.qualifiedName + " " + srg.name + " # " + parent.name
+                    classSrg + " " + srg.name + " # " + parent.name
                 )
             }
             is PsiMethod -> {
+                if (parent.containingClass == null)
+                    return showBalloon("No SRG name found", e)
+                val classSrg = srgMap.getSrgClass(parent.containingClass!!) ?: return showBalloon("No SRG name found", e)
                 val srg = srgMap.getSrgMethod(parent) ?: return showBalloon("No SRG name found", e)
                 copyToClipboard(
                     data.editor,
                     data.element,
-                    parent.containingClass?.qualifiedName + " " + srg.name + srg.descriptor + " # " + parent.name
+                    classSrg + " " + srg.name + srg.descriptor + " # " + parent.name
                 )
             }
             is PsiClass -> {
