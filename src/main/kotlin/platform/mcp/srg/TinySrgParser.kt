acbbffe5a412c6f720f078b2256322c172f2ac59
RedNesto <RedNestoMC@gmail.com>
2022-06-08 18:03:46 +0200

    Implement new TSRG parser based on mapping-io
    Fixes #1849

--- src/main/kotlin/platform/mcp/srg/TinySrgParser.kt
+++ src/main/kotlin/platform/mcp/srg/TinySrgParser.kt
@@ -12,8 +12,12 @@ package com.demonwav.mcdev.platform.mcp.srg
 
 import com.demonwav.mcdev.util.MemberReference
 import com.google.common.collect.ImmutableBiMap
+import com.intellij.openapi.util.registry.Registry
 import java.nio.file.Files
 import java.nio.file.Path
+import net.fabricmc.mappingio.MappedElementKind
+import net.fabricmc.mappingio.MappingReader
+import net.fabricmc.mappingio.MappingVisitor
 
 object TinySrgParser : SrgParser {
     private val commentRegex = Regex("#.+")
@@ -24,6 +28,27 @@ object TinySrgParser : SrgParser {
         val methodMapBuilder = ImmutableBiMap.builder<MemberReference, MemberReference>()
         val srgNames = hashMapOf<String, String>()
 
+        if (Registry.`is`("mcdev.new.tsrg.parser")) {
+            newParser(path, classMapBuilder, fieldMapBuilder, methodMapBuilder, srgNames)
+        } else {
+            oldParser(path, classMapBuilder, fieldMapBuilder, methodMapBuilder, srgNames)
+        }
+
+        return McpSrgMap(
+            classMapBuilder.build(),
+            fieldMapBuilder.build(),
+            methodMapBuilder.build(),
+            srgNames
+        )
+    }
+
+    private fun oldParser(
+        path: Path,
+        classMapBuilder: ImmutableBiMap.Builder<String, String>,
+        fieldMapBuilder: ImmutableBiMap.Builder<MemberReference, MemberReference>,
+        methodMapBuilder: ImmutableBiMap.Builder<MemberReference, MemberReference>,
+        srgNames: HashMap<String, String>
+    ) {
         var classRef: Pair<String, String>? = null
 
         Files.lines(path).forEach { line ->
@@ -61,12 +86,85 @@ object TinySrgParser : SrgParser {
                 srgNames[srg] = mcp
             }
         }
+    }
 
-        return McpSrgMap(
-            classMapBuilder.build(),
-            fieldMapBuilder.build(),
-            methodMapBuilder.build(),
-            srgNames
-        )
+    private fun newParser(
+        path: Path,
+        classMapBuilder: ImmutableBiMap.Builder<String, String>,
+        fieldMapBuilder: ImmutableBiMap.Builder<MemberReference, MemberReference>,
+        methodMapBuilder: ImmutableBiMap.Builder<MemberReference, MemberReference>,
+        srgNames: HashMap<String, String>
+    ) {
+        val visitor = object : MappingVisitor {
+            var namedNsIndex = -1
+
+            lateinit var cls: String
+            var desc: String? = null
+            var inter: String? = null
+            var named: String? = null
+
+            override fun visitNamespaces(srcNamespace: String, dstNamespaces: MutableList<String>) {
+                namedNsIndex = dstNamespaces.indexOf("right")
+            }
+
+            override fun visitContent(): Boolean {
+                return namedNsIndex >= 0
+            }
+
+            override fun visitClass(srcName: String): Boolean {
+                cls = srcName.replace('/', '.')
+                desc = null
+                inter = null
+                named = null
+                return true
+            }
+
+            override fun visitField(srcName: String, srcDesc: String?): Boolean {
+                inter = srcName
+                return true
+            }
+
+            override fun visitMethod(srcName: String, srcDesc: String): Boolean {
+                inter = srcName
+                desc = srcDesc
+                return true
+            }
+
+            override fun visitMethodArg(argPosition: Int, lvIndex: Int, srcName: String): Boolean {
+                return false
+            }
+
+            override fun visitMethodVar(lvtRowIndex: Int, lvIndex: Int, startOpIdx: Int, srcName: String): Boolean {
+                return false
+            }
+
+            override fun visitDstName(targetKind: MappedElementKind, namespace: Int, name: String) {
+                when (namespace) {
+                    namedNsIndex -> named = name
+                    else -> return
+                }
+
+                if (targetKind == MappedElementKind.CLASS && named != null) {
+                    classMapBuilder.put(cls, named!!.replace('/', '.'))
+                } else if (inter != null && named != null) {
+                    if (targetKind == MappedElementKind.FIELD) {
+                        fieldMapBuilder.put(MemberReference(inter!!, null, cls), MemberReference(named!!, null, cls))
+                        srgNames[inter!!] = named!!
+                    } else if (targetKind == MappedElementKind.METHOD) {
+                        methodMapBuilder.put(MemberReference(inter!!, desc, cls), MemberReference(named!!, desc, cls))
+                        srgNames[inter!!] = named!!
+                    }
+                }
+            }
+
+            override fun visitComment(targetKind: MappedElementKind, comment: String) {
+            }
+
+            override fun visitElementContent(targetKind: MappedElementKind?): Boolean {
+                return targetKind != MappedElementKind.METHOD
+            }
+        }
+
+        MappingReader.read(path, visitor)
     }
 }
