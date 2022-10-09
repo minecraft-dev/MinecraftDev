d50a77a8d094c2cc1baac65746bda8544abc7379
l-Luna <izzeldeen03@gmail.com>
2022-10-09 15:19:31 +0100

    "Copy AW entry" fix for inaccessible references in fabric projects

--- src/main/kotlin/platform/mcp/actions/CopyAwAction.kt
+++ src/main/kotlin/platform/mcp/actions/CopyAwAction.kt
@@ -32,6 +32,7 @@ class CopyAwAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val data = getDataFromActionEvent(e) ?: return showBalloon("Unknown failure", e)
+        val editor = data.editor
 
         val element = data.element
         if (element !is PsiIdentifier) {
@@ -45,33 +46,48 @@ class CopyAwAction : AnAction() {
             else -> null
         } ?: return showBalloon("Invalid element", e)
 
-        when (target) {
-            is PsiClass -> {
-                val text = "accessible class ${target.internalName}"
-                copyToClipboard(data.editor, element, text)
-            }
-            is PsiField -> {
-                val containing = target.containingClass?.internalName
-                    ?: return showBalloon("Could not get owner of field", e)
-                val desc = target.type.descriptor
-                val text = "accessible field $containing ${target.name} $desc"
-                copyToClipboard(data.editor, element, text)
-            }
-            is PsiMethod -> {
-                val containing = target.containingClass?.internalName
-                    ?: return showBalloon("Could not get owner of method", e)
-                val desc = target.descriptor ?: return showBalloon("Could not get descriptor of method", e)
-                val text = "accessible method $containing ${target.name} $desc"
-                copyToClipboard(data.editor, element, text)
-            }
-            else -> showBalloon("Invalid element", e)
-        }
+        doCopy(target, element, editor, e)
     }
 
-    private fun copyToClipboard(editor: Editor, element: PsiElement, text: String) {
-        val stringSelection = StringSelection(text)
-        val clpbrd = Toolkit.getDefaultToolkit().systemClipboard
-        clpbrd.setContents(stringSelection, null)
-        showSuccessBalloon(editor, element, "Copied: \"$text\"")
+    companion object {
+
+        fun doCopy(target: PsiElement, element: PsiElement, editor: Editor?, e: AnActionEvent?) {
+            when (target) {
+                is PsiClass -> {
+                    val text = "accessible class ${target.internalName}"
+                    copyToClipboard(editor, element, text)
+                }
+                is PsiField -> {
+                    val containing = target.containingClass?.internalName
+                        ?: return maybeShow("Could not get owner of field", e)
+                    val desc = target.type.descriptor
+                    val text = "accessible field $containing ${target.name} $desc"
+                    copyToClipboard(editor, element, text)
+                }
+                is PsiMethod -> {
+                    val containing = target.containingClass?.internalName
+                        ?: return maybeShow("Could not get owner of method", e)
+                    val desc = target.descriptor ?: return maybeShow("Could not get descriptor of method", e)
+                    val text = "accessible method $containing ${target.name} $desc"
+                    copyToClipboard(editor, element, text)
+                }
+                else -> maybeShow("Invalid element", e)
+            }
+        }
+
+        private fun copyToClipboard(editor: Editor?, element: PsiElement, text: String) {
+            val stringSelection = StringSelection(text)
+            val clpbrd = Toolkit.getDefaultToolkit().systemClipboard
+            clpbrd.setContents(stringSelection, null)
+            if (editor != null) {
+                showSuccessBalloon(editor, element, "Copied: \"$text\"")
+            }
+        }
+
+        private fun maybeShow(text: String, e: AnActionEvent?) {
+            if (e != null) {
+                showBalloon(text, e)
+            }
+        }
     }
 }
