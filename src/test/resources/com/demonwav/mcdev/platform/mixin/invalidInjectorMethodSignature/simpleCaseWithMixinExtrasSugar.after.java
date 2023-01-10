9aaac3a1e398b3b2acd6ece8eff92af5877a89f6
LlamaLad7 <l3gomindstorms@gmail.com>
2023-01-10 23:11:58 +0000

    Add a test for MixinExtras Sugar.

---
+++ src/test/resources/com/demonwav/mcdev/platform/mixin/invalidInjectorMethodSignature/simpleCaseWithMixinExtrasSugar.after.java
@@ -0,0 +1,16 @@
+package test;
+
+import com.demonwav.mcdev.mixintestdata.invalidInjectorMethodSignatureFix.MixedInSimple;
+import com.llamalad7.mixinextras.sugar.Local;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MixedInSimple.class)
+public class TestMixin {
+
+    @Inject(method = "simpleMethod", at = @At("RETURN"))
+    private void injectCtor(String string, int i, CallbackInfo ci, @Local String str<caret>) {
+    }
+}
