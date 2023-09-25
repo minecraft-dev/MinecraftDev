44be6035e108d2f28cd185494856420ac6dc0613
Joe <burtonjae@hotmail.co.uk>
2023-09-25 02:36:30 +0100

    Fix formatting

--- src/main/kotlin/translations/identification/TranslationIdentifier.kt
+++ src/main/kotlin/translations/identification/TranslationIdentifier.kt
@@ -69,8 +69,8 @@ abstract class TranslationIdentifier<T : PsiElement> {
 
             val method = call.referencedMethod ?: return null
             val parameter = method.parameterList.getParameter(index) ?: return null
-            val translatableAnnotation = AnnotationUtil.findAnnotation(parameter, TranslationConstants.TRANSLATABLE_ANNOTATION)
-                ?: return null
+            val translatableAnnotation =
+                AnnotationUtil.findAnnotation(parameter, TranslationConstants.TRANSLATABLE_ANNOTATION) ?: return null
 
             val prefix =
                 translatableAnnotation.findAttributeValue(TranslationConstants.PREFIX)?.constantStringValue ?: ""
