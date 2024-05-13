120a66f21ce24e5b283428e361e1fa7afce0839c
RedNesto <RedNestoMC@gmail.com>
2024-05-13 18:02:40 +0200

    Add default values to template descriptor

--- src/main/kotlin/creator/custom/TemplateDescriptor.kt
+++ src/main/kotlin/creator/custom/TemplateDescriptor.kt
@@ -9,22 +9,23 @@ data class TemplatePropertyDescriptor(
     val name: String,
     val type: String,
     val label: String,
-    val order: Int?,
-    val options: List<Any>?,
-    val remember: Boolean?,
-    val hidden: Boolean?,
-    val editable: Boolean?,
+    val order: Int? = null,
+    val options: List<Any>? = null,
+    val groupProperties: List<TemplatePropertyDescriptor>? = null,
+    val remember: Boolean? = null,
+    val hidden: Boolean? = null,
+    val editable: Boolean? = null,
     val default: Any,
-    val derives: PropertyDerivation?,
-    val inheritFrom: String?
+    val derives: PropertyDerivation? = null,
+    val inheritFrom: String? = null
 )
 
 data class PropertyDerivation(
-    val parents: List<String>?,
+    val parents: List<String>? = null,
     val method: String,
     val default: Any,
-    val whenModified: Boolean?,
-    val parameters: Map<String, Any?>?,
+    val whenModified: Boolean? = null,
+    val parameters: Map<String, Any?>? = null,
 )
 
 data class TemplateFile(
