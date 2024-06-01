897baf726ac04a10f8fae79b892e986dbaf31478
RedNesto <RedNestoMC@gmail.com>
2024-06-01 20:58:43 +0200

    Some ClassFqn doc & withClassName

--- src/main/kotlin/creator/custom/model/ClassFqn.kt
+++ src/main/kotlin/creator/custom/model/ClassFqn.kt
@@ -3,10 +3,27 @@ package com.demonwav.mcdev.creator.custom.model
 @TemplateApi
 data class ClassFqn(val fqn: String) {
 
+    /**
+     * The [Class.simpleName] of this class.
+     */
     val className by lazy { fqn.substringAfterLast('.') }
+
+    /**
+     * The relative filesystem path to this class, without extension.
+     */
     val path by lazy { fqn.replace('.', '/') }
+
+    /**
+     * The package name of this FQN as it would appear in source code.
+     */
     val packageName by lazy { fqn.substringBeforeLast('.') }
+
+    /**
+     * The package path of this FQN reflected as a local filesystem path
+     */
     val packagePath by lazy { packageName.replace('.', '/') }
+
+    fun withClassName(className: String) = copy("$packageName.$className")
 
     override fun toString(): String = fqn
 }
