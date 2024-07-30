28b38c1536d4301ad318af288a05a96d29ce3c50
Kikugie <kikugie@duck.com>
2024-07-30 15:01:32 +0200

    Remove outlines

--- src/main/kotlin/asset/MixinAssets.kt
+++ src/main/kotlin/asset/MixinAssets.kt
@@ -29,5 +29,4 @@ object MixinAssets : Assets() {
     val MIXIN_CLASS_ICON_DARK = loadIcon("/assets/icons/mixin/mixin_class_gutter_dark.png")
 
     val MIXIN_MARK = loadIcon("/assets/icons/mixin/mixin_mark.svg")
-    val MIXIN_MARK_DARK = loadIcon("/assets/icons/mixin/mixin_mark_dark.svg")
 }
