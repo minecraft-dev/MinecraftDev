afda36bebae6fe637bba893df9835b7534c31022
Strokkur24 <strokkur.24@gmail.com>
2026-04-05 23:00:11 +0200

    Replace Paper build check with Maven version range string

--- src/main/kotlin/creator/custom/derivation/FetchPaperDependencyVersionForMcVersion.kt
+++ src/main/kotlin/creator/custom/derivation/FetchPaperDependencyVersionForMcVersion.kt
@@ -23,19 +23,8 @@ package com.demonwav.mcdev.creator.custom.derivation
 import com.demonwav.mcdev.creator.custom.PropertyDerivation
 import com.demonwav.mcdev.creator.custom.TemplateValidationReporter
 import com.demonwav.mcdev.creator.custom.types.CreatorProperty
-import com.demonwav.mcdev.update.PluginUtil
 import com.demonwav.mcdev.util.MinecraftVersions
 import com.demonwav.mcdev.util.SemanticVersion
-import io.ktor.client.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.int
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
 
 class FetchPaperDependencyVersionForMcVersion : PreparedDerivation {
 
@@ -46,40 +35,12 @@ class FetchPaperDependencyVersionForMcVersion : PreparedDerivation {
         }
 
         val isMaven = (parentValues[1] as String) == "Maven"
-        if (isMaven) {
-            val latestBuild = fetchPaperBuildData(version.toString());
-            return "${version}.build.${latestBuild.id}-${latestBuild.channel}"
-        }
-        return "${version}.build.+"
-    }
-
-    private fun fetchPaperBuildData(version: String): PaperBuild {
-        return runBlocking {
-            val client = HttpClient()
-            val response = client.get("https://fill.papermc.io/v3/projects/paper/versions/${version}/builds", block = {
-                this.header(
-                    "User-Agent",
-                    "minecraft-dev/${PluginUtil.pluginVersion} (https://github.com/minecraft-dev/MinecraftDev)"
-                )
-            })
-            if (response.status.isSuccess()) {
-                return@runBlocking Json.parseToJsonElement(response.bodyAsText()).jsonArray[0]
-                    .let { build ->
-                        return@let PaperBuild(
-                            build.jsonObject["id"]!!.jsonPrimitive.int,
-                            build.jsonObject["channel"]!!.jsonPrimitive.content.lowercase()
-                        )
-                    }
-            } else {
-                throw IllegalStateException("Failed to fetch latest Paper build for version ${version}: ${response.bodyAsText()}")
-            }
+        return if (isMaven) {
+            "[${version}.build,)"
+        } else {
+            "${version}.build.+"
         }
     }
-
-    private data class PaperBuild(
-        val id: Int,
-        val channel: String,
-    );
 
     companion object : PropertyDerivationFactory {
 
